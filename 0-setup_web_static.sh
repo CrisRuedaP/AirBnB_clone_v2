22917461173b70dae9d94986fa607121f2054d46
CrisRuedaP <crisruedap@gmail.com>
2020-08-17 23:03:10 +0000

    Adding command to create file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,6 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
