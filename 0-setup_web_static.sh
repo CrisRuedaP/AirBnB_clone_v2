c23390c43d64f5cf97140173f92dfb27dd393c34
CrisRuedaP <crisruedap@gmail.com>
2020-08-17 23:58:45 +0000

    adding new flag to the permission

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,6 @@ sudo mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i "35i $file" /etc/nginx/sites-available/default
 sudo service nginx restart
