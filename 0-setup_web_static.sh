fe2cb2344c779864453bf3b0a3bd166976718214
CrisRuedaP <crisruedap@gmail.com>
2020-08-17 22:51:54 +0000

    Fixing path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,14 +5,8 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "35i $file" etc/nginx/sites-enabled/default
+sudo sed -i "35i $file" /etc/nginx/sites-enabled/default
 sudo service nginx restart
