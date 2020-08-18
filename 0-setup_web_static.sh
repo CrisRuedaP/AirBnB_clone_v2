fca1890831e277813fde887303a5af77b7b26805
CrisRuedaP <crisruedap@gmail.com>
2020-08-18 00:02:25 +0000

    fuck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ file="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo mkdir /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
