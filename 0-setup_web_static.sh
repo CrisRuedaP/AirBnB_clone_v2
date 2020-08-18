499021c639613e124e52d6ab86c792a2173c21b2
CrisRuedaP <crisruedap@gmail.com>
2020-08-18 00:09:22 +0000

    trying more things

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,11 +5,11 @@ file="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir /data/web_static/releases/test/
-sudo mkdir /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "35i \ $file" /etc/nginx/sites-enabled/default
+sudo sed -i "35i \ $file" /etc/nginx/sites-available/default
 sudo service nginx restart
